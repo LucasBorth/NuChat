ae2cee2395a3c63baa12a86ae6c37bb9a8423ebc
Jeú Chaves Lima <jeuchaveslima@gmail.com>
2021-06-27 16:12:06 -0300

    Alteracao Tela Editar Perfil

--- app/view/pgEditarPerfil.php
+++ app/view/pgEditarPerfil.php
@@ -12,33 +12,23 @@
     <div class="align-center middle-margin">
         <div class="card shadow middle-padding" style="width: 65%;">
             <div class="align-center">
-                <img src="files/icon.svg" alt="Logo" id="logo">
+                <img src="../../images/logo.svg" alt="Logo" id="logo">
                 <h4 class="purple-font">NuChat</h4>
             </div>
             <div class="align-center">
-                <h2>Editar sobre</h2>
+                <h2>Editar Perfil</h2>
             </div>
             <div class="align-center">
                 <div class="small-margin">
                     <form>
                         <input type="text" id="login" name="name" placeholder="Digite seu nome">
-                        <input type="text" id="job" name="job" placeholder="Digite seu trabalho">
-                        <input type="text" id="study" name="study" placeholder="Digite onde estuda">
-                        <input type="text" id="home" name="home" placeholder="Digite onde mora">
-                    </form>
-                </div>
-                <div class="small-margin">
-                    <form>
-                        <input type="text" id="birth" name="birth" placeholder="Digite onde nasceu">
-                        <input type="text" id="birth-date" name="birth-date" placeholder="Digite quando nasceu">
-                        <input type="text" id="gender" name="gender" placeholder="Digite seu gênero">
-                        <input type="text" id="relationship" name="relationship" placeholder="Digite seu estado de relacionamento">
+                        <input type="text" id="login" name="name" placeholder="Digite sua nova senha">
                     </form>
                 </div>
             </div>
             <div class="align-center small-margin">
-                <a href="pgPerfil.php"><button class="secondary-button">Cancelar</button></a>
-                <a href="pgPerfil.php"><button class="primary-button">Salvar</button></a>
+                <a href="pgConversar.php"><button class="secondary-button">Cancelar</button></a>
+                <a href="pgConversar.php"><button class="primary-button">Salvar</button></a>
             </div>
         </div>
     </div>
