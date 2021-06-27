d53780203978889f09d9e0c0a65edb847990befa
Jeú Chaves Lima <jeuchaveslima@gmail.com>
2021-06-27 17:51:03 -0300

    Alteracao Controlador login e cadastrar

--- app/controller/Logar.php
+++ app/controller/Logar.php
@@ -10,29 +10,17 @@
         echo "<script>window.location.href = '../pgLogin.php';</script>";
     }
 
-    include("conexao.php");
+    include_once("../model/Usuario.php");
 
     //Verificacao de existencia de usuario
-    $sql = "SELECT nome FROM usuarios WHERE email = '$email' AND senha = '$senha';";
-    $result = mysqli_query($conexao, $sql);
-
-    include("fimDeConexao.php");
-    
-    if (mysqli_num_rows($result) >= 1){
-        $linha = mysqli_fetch_assoc($result);
-
-        //Iniciar Sessao
+    if(!is_null(Usuario::buscarPorEmail($email))) {
         session_start();
-
         $_SESSION["login"] = true;
-        $_SESSION["nome"] = $linha["nome"];
-
-        echo "<script>window.location.href = '../pgChat.php';</script>";
-    } else{
+        $_SESSION["nome_usuario"] = $linha["nome_usuario"];
+    } else {
         $_SESSION["tryLogin"] = true;
-
         echo "<script>alert('Login Incorreto, tente novamente!')</script>";
-
-        echo "<script>window.location.href = '../pgLogin.php';</script>";
+        echo "<script>window.location.href = '../pgLogar.php';</script>";
     }
+    
 ?>
